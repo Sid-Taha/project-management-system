a3484616fba362a28d175f9289254dde461f1fea
Sid-Taha <twahasiddqui11@gmail.com>
2026-01-06 09:00:32 +0500

    feat: add email functionality with Mailgen and Nodemailer
    - Implemented email sending capabilities using Mailgen and Nodemailer.
    - Added methods for generating email content for verification and password reset.
    - Updated package.json and package-lock.json to include new dependencies: mailgen and nodemailer.
    - Refactored user model to include a method for generating temporary tokens for email verification and password reset.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,57 +1,9 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema({
-    avatar: {
-        type: {
-            url : String,
-        },
-        default: {
-            url : "https://placehold.co/200x200",
-        }
-    },
-    username: {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    fullName: {
-        type: String,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-    },
-    isEmailVerified: {
-        type: Boolean,
-        default: false,
-    },
-    refreshToken: {
-        type: String,
-    },
-    forgotPasswordToken: {
-        type: String,
-    },
-    forgotPasswordTokenExpiry: {
-        type: Date,
-    },
-    emailVerificationToken: {
-        type: String,
-    },
-    emailVerificationTokenExpiry: {
-        type: Date,
-    }
-}, {timestamps: true})
+
 
 // -------------------------------- Pre Hooks -------------------------------- //
 userSchema.pre("save", async function(next){
@@ -65,11 +17,14 @@ userSchema.pre("save", async function(next){
     } 
 })
 
+
+
 // -------------------------------- Methods -------------------------------- //
 //------------------------------- Compare Password
 userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
+
 
 //------------------------------- Generate JWT Access Token
 userSchema.methods.generateAccessToken = function (){
@@ -87,18 +42,37 @@ userSchema.methods.generateAccessToken = function (){
     )
 }
 
+
 // ------------------------------- Generate JWT Refresh Token
 userSchema.methods.generateRefreshToken = function (){
     const payload = {
         _id: this._id,
     }
-
+    
     return jwt.sign( 
         payload, 
         process.env.REFRESH_TOKEN_SECRET, 
         {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
     )
 }
+
+
+// ------------------------------- temporary tokens for email verification and password reset
+userSchema.methods.generateTemporaryToken = function (){
+    const unHashedToken = crypto.randomBytes(20).toString('hex');
+
+    const hashedToken = crypto
+        .createHash('sha256')
+        .update(unHashedToken)
+        .digest('hex');
+
+    const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 minutes from now
+
+    return {unHashedToken, hashedToken, tokenExpiry};
+}
+
+
+
 
 
 const userTable = mongoose.model("User", userSchema);
---
+++ src/utils/mail.js
@@ -0,0 +1,90 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Project Manager",
+      link: "https://taskmanagelink.com",
+    },
+  });
+
+  // email in text format
+  const emailText = mailGenerator.generatePlaintext(options.mailgenContent);
+
+  // email in HTML format
+  const emailHTML = mailGenerator.generate(options.mailgenContent);
+
+  // creating transport for sending email
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+
+    // defining email options
+    const mail = {
+        from: 'mail.taskmanager@example.com',
+        to: options.email,
+        subject: options.subject,
+        text: emailText,
+        html: emailHTML,
+    };
+    
+    // sending email
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error("❌ Error sending email:", error);
+    }
+
+
+};
+
+// ----------------------------------- Email Verification Mailgen Content --------------------------
+const emailVerificationMailgenContent = (username, verificationURL) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! We're very excited to have you on board.",
+      action: {
+        instructions:
+          "Please click the following button to verify your account:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Confirm your account",
+          link: verificationURL,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+// ----------------------------------- Forget Password Mailgen Content --------------------------
+const forgetPasswordMailgenContent = (username, passwordResetURL) => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "You have requested to reset your password. Please click the button below to proceed.",
+      action: {
+        instructions: "Click the button below to reset your password:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Reset your password",
+          link: passwordResetURL,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export { emailVerificationMailgenContent, forgetPasswordMailgenContent };
