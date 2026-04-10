e415b03f968dd251d25f300d337d08de6859dc0b
Sid-Taha <twahasiddqui11@gmail.com>
2026-04-10 21:51:53 +0500

    feat: add uploadTaskAttachments middleware for handling task file uploads

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -5,6 +5,21 @@ import s3_Client from "../config/s3.js"
 
 dotenv.config()
 
+const allowedMimeTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "application/pdf",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/zip"
+]
+
+
+
+
 export const uploadAvatar = multer({
     storage: multerS3({
         s3: s3_Client,
@@ -17,9 +32,45 @@ export const uploadAvatar = multer({
             cb(null, filename)
         }
     }),
-    fileFilter,
+    
+    fileFilter: (req, file, cb)=>{
+        // only avatar image is allow
+        const imageTypes = ["image/jpeg", "image/png", "image/gif"]
+        if(imageTypes.includes(file.mimetype)){
+            cb(null, true)
+        }else{
+            cb(new Error("only image are allowed for avatar", false))
+        }
+    },
+
+
     limits: {
         fileSize: 50 * 1024 * 1024, //50Mb
         files: 10 // max 10 files
     }
+})
+
+
+
+
+
+export const uploadTaskAttachments = multer({
+    storage: multerS3({
+        s3: s3_Client,
+        bucket: process.env.AWS_S3_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb)=>{
+            const taskId = req.param.taskId || "new"
+            const timestamp = Date.now()
+            const filename = `task-${taskId}/${timestamp}-${file.originalname}`
+            cb(null, filename)
+        } 
+    }),
+    fileFilter: (req, file, cb)=>{
+        if(allowedMimeTypes.includes(file.mimetype)){
+            cb(null, true)
+        }else{
+            cb(new Error("File type not allowed", false))
+        }
+    },
 })
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,6 +2,7 @@ import express from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { requireProjectAdmin, requireProjectMember } from "../middlewares/project.middleware.js"
 import { createTask, listTask, getTaskDetails, updateTask, deleteTask } from "../controllers/task.controllers.js"
+import { uploadTaskAttachments } from "../middlewares/upload.middleware.js"
 
 
 const router = express.Router()
@@ -10,7 +11,7 @@ const router = express.Router()
 router.use(verifyJWT) // checking your login status
 
 
-router.route("/:projectId").all(requireProjectMember, requireProjectAdmin).post(createTask) // create task
+router.route("/:projectId").all(requireProjectMember, uploadTaskAttachments.array("attachments", 10), requireProjectAdmin).post(createTask) // create task
 
 router.route("/:projectId").all(requireProjectMember).get(listTask) // list all task
 
